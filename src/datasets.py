aeae0eb3b53eb141c4d74e452d9888f6d4f22825
fabio <fabsalienzoo@gmail.com>
2020-02-20 17:14:38 +0000

    Code refactoring to enable pytorch transforms on smallnorb

--- src/datasets.py
+++ src/datasets.py
@@ -3,29 +3,28 @@ import torchvision
 import numpy as np
 import scipy.io as sio
 
+from utils import *
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 
-from smallNORB_transforms import *
-
 def smallnorb(args, dataset_paths):
 
-    transf = {'train': Compose([
-                ToPILImage(),
-                RandomCrop((args.crop_dim, args.crop_dim)),
-                ColorJitter(brightness=args.brightness/255., contrast=args.contrast),
-                ToTensor(),
-                smallnorbStandardize()]),
-                # Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))]),
-        'test':  Compose([
-                ToPILImage(),
-                CenterCrop((args.crop_dim, args.crop_dim)),
-                ToTensor(),
-                smallnorbStandardize()])}
-                # Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))])}
+    transf = {'train': transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.RandomCrop((args.crop_dim, args.crop_dim)),
+                transforms.ColorJitter(brightness=args.brightness/255., contrast=args.contrast),
+                transforms.ToTensor(),
+                Standardize()]),
+                # transforms.Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))]),
+        'test':  transforms.Compose([
+                transforms.ToPILImage(),
+                transforms.CenterCrop((args.crop_dim, args.crop_dim)),
+                transforms.ToTensor(),
+                Standardize()])}
+                # transforms.Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))])}
 
     config = {'train': True, 'test': False}
-    datasets = {i: DatasetLoader(dataset_paths[i], transform=transf[i],
+    datasets = {i: smallNORB(dataset_paths[i], transform=transf[i],
         shuffle=config[i]) for i in config.keys()}
 
     # return data, labels dicts for new train set and class-balanced valid set
@@ -36,31 +35,31 @@ def smallnorb(args, dataset_paths):
             datasets['train'].labels, return_counts=True)[1] // 5) # % of train set per class
 
     # define transforms for train set (without valid data)
-    transf['train_'] = Compose([
-            ToPILImage(),
-            RandomCrop((args.crop_dim, args.crop_dim)),
-            ColorJitter(brightness=args.brightness/255., contrast=args.contrast),
-            ToTensor(),
-            smallnorbStandardize()])
-            # Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))])
+    transf['train_'] = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.RandomCrop((args.crop_dim, args.crop_dim)),
+            transforms.ColorJitter(brightness=args.brightness/255., contrast=args.contrast),
+            transforms.ToTensor(),
+            Standardize()])
+            # transforms.Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))])
 
     # define transforms for class-balanced valid set
-    transf['valid'] = Compose([
-            ToPILImage(),
-            CenterCrop((args.crop_dim, args.crop_dim)),
-            ToTensor(),
-            smallnorbStandardize()])
-            # Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))])
+    transf['valid'] = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.CenterCrop((args.crop_dim, args.crop_dim)),
+            transforms.ToTensor(),
+            Standardize()])
+            # transforms.Normalize((0.75239172, 0.75738262), (0.1758033 , 0.17200065))])
 
     # save original full training set
     datasets['train_valid'] = datasets['train']
 
     # make new training set without validation samples
-    datasets['train'] = smallNORBDataset(data=data['train'],
+    datasets['train'] = CustomDataset(data=data['train'],
         labels=labels['train'], transform=transf['train_'])
 
     # make class balanced validation set
-    datasets['valid'] = smallNORBDataset(data=data['valid'],
+    datasets['valid'] = CustomDataset(data=data['valid'],
         labels=labels['valid'], transform=transf['valid'])
 
     config = {'train': True, 'train_valid': True,
@@ -71,7 +70,7 @@ def smallnorb(args, dataset_paths):
 
     return dataloaders
 
-class DatasetLoader(Dataset):
+class smallNORB(Dataset):
     ''' In:
             data_path (string): path to the dataset split folder, i.e. train/valid/test
             transform (callable, optional): transform to be applied on a sample.
@@ -91,10 +90,14 @@ class DatasetLoader(Dataset):
 
             # get name of each file per class and respective class name/label index
             for _, file_name in enumerate(os.listdir(class_path)):
-                self.data.extend([np.load(os.path.join(data_path, class_name, file_name))])
+                img = np.load(os.path.join(data_path, class_name, file_name))
+                # Out ← [H, W, C] ← [C, H, W]
+                if img.shape[0] < img.shape[1]:
+                    img = np.moveaxis(img, 0, -1)
+                self.data.extend([img])
                 self.labels.append(class_label_idx)
 
-        self.data = np.array(self.data)
+        self.data = np.array(self.data, dtype=np.uint8)
         self.labels = np.array(self.labels)
 
         if self.shuffle:
@@ -108,31 +111,10 @@ class DatasetLoader(Dataset):
 
     def __getitem__(self, idx):
 
-        image = self.data[idx]
-
-        # transform each instance independently per channel
         if self.transform:
-            image0, image1 = self.transform(image[0], image[1])
+            image = self.transform(self.data[idx])
 
-        return torch.cat((image0, image1)), self.labels[idx] # (X, Y)
-
-class smallNORBDataset(Dataset):
-    def __init__(self, data, labels, transform):
-        self.data = np.array(data)
-        self.labels = np.array(labels)
-        self.transform = transform
-
-        # shuffle the dataset
-        idx = np.random.permutation(self.data.shape[0])
-        self.data = self.data[idx]
-        self.labels = self.labels[idx]
-
-    def __len__(self):
-        return self.data.shape[0]
-
-    def __getitem__(self, idx):
-        img0, img1 = self.transform(self.data[idx][0], self.data[idx][1])
-        return torch.cat((img0, img1)), self.labels[idx]
+        return image, self.labels[idx] # (X, Y)
 
 def mnist(args, dataset_paths):
 
@@ -156,6 +138,9 @@ def mnist(args, dataset_paths):
         labels=datasets['train'].targets,
         n_classes=args.arch[-1],
         n_samples_per_class=np.repeat(500, args.arch[-1])) # 500 per class
+
+    print(datasets['train'].data.type)
+    # print(data['train'].type)
 
     # define transforms for train set (without valid data)
     transf['train_'] = transforms.Compose([
@@ -265,42 +250,3 @@ class affNIST(Dataset):
             image = self.transform(image)
 
         return image, self.labels[idx] # (X, Y)
-
-class CustomDataset(Dataset):
-    def __init__(self, data, labels, transform):
-        # shuffle the dataset
-        idx = np.random.permutation(data.shape[0])
-        self.data = data[idx]
-        self.labels = labels[idx]
-        self.transform = transform
-
-    def __len__(self):
-        return self.data.shape[0]
-
-    def __getitem__(self, idx):
-        return self.transform(self.data[idx]), self.labels[idx]
-
-def random_split(data, labels, n_classes, n_samples_per_class):
-    ''' create a class-balanced validation set '''
-    train_X, train_Y, valid_X, valid_Y = [],[],[],[]
-
-    for c in range(n_classes):
-        # get indices of all class 'c' samples
-        c_idx = (np.array(labels) == c).nonzero()[0]
-        # get n unique class 'c' samples
-        valid_samples = np.random.choice(c_idx, n_samples_per_class[c], replace=False)
-        # get remaining samples of class 'c'
-        train_samples = np.setdiff1d(c_idx, valid_samples)
-        # assign class c samples to validation, and remaining to training
-        train_X.extend(data[train_samples])
-        train_Y.extend(labels[train_samples])
-        valid_X.extend(data[valid_samples])
-        valid_Y.extend(labels[valid_samples])
-
-    if isinstance(data, torch.Tensor):
-        # .stack transforms list of tensors to tensor
-        return {'train': torch.stack(train_X), 'valid': torch.stack(valid_X)}, \
-            {'train': torch.stack(train_Y), 'valid': torch.stack(valid_Y)}
-    else:
-        return {'train': train_X, 'valid': valid_X}, \
-            {'train': train_Y, 'valid': valid_Y}
---
+++ src/utils.py
@@ -0,0 +1,57 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset
+
+class CustomDataset(Dataset):
+    """ Creates a custom pytorch dataset, mainly
+        used for creating validation set splits. """
+    def __init__(self, data, labels, transform):
+        # shuffle the dataset
+        idx = np.random.permutation(data.shape[0])
+        self.data = data[idx]
+        self.labels = labels[idx]
+        self.transform = transform
+
+    def __len__(self):
+        return self.data.shape[0]
+
+    def __getitem__(self, idx):
+        return self.transform(self.data[idx]), self.labels[idx]
+
+class Standardize(object):
+    """ Standardizes a 'PIL Image' such that each channel
+        gets zero mean and unit variance. """
+    def __call__(self, img):
+        return (img - img.mean(dim=(1,2), keepdim=True)) \
+            / torch.clamp(img.std(dim=(1,2), keepdim=True), min=1e-8)
+
+    def __repr__(self):
+        return self.__class__.__name__ + '()'
+
+def random_split(data, labels, n_classes, n_samples_per_class):
+    """ Creates a class-balanced validation set from a training set. """
+    train_X, train_Y, valid_X, valid_Y = [],[],[],[]
+
+    for c in range(n_classes):
+        # get indices of all class 'c' samples
+        c_idx = (np.array(labels) == c).nonzero()[0]
+        # get n unique class 'c' samples
+        valid_samples = np.random.choice(c_idx, n_samples_per_class[c], replace=False)
+        # get remaining samples of class 'c'
+        train_samples = np.setdiff1d(c_idx, valid_samples)
+        # assign class c samples to validation, and remaining to training
+        train_X.extend(data[train_samples])
+        train_Y.extend(labels[train_samples])
+        valid_X.extend(data[valid_samples])
+        valid_Y.extend(labels[valid_samples])
+
+    if isinstance(data, torch.Tensor):
+        # torch.stack transforms list of tensors to tensor
+        return {'train': torch.stack(train_X), 'valid': torch.stack(valid_X)}, \
+            {'train': torch.stack(train_Y), 'valid': torch.stack(valid_Y)}
+    else:
+        # transforms list of np arrays to tensor
+        return {'train': torch.from_numpy(np.stack(train_X)), \
+                'valid': torch.from_numpy(np.stack(valid_X))}, \
+            {'train': torch.from_numpy(np.stack(train_Y)), \
+             'valid': torch.from_numpy(np.stack(valid_Y))}
